4b53c05e6e6e63a5db81467c21de4c06b77b2a4f
F3224438 Fernando Campos Schelb <schelb@bb.com.br>
2020-08-11 17:01:53 -0300

    add simple employee in mariaDB

--- router/routers.go
+++ router/routers.go
@@ -1,15 +1,25 @@
 package routers
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
+	"github.com/cubcoffee/valhalla-api/dao"
+	"github.com/cubcoffee/valhalla-api/model"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateRouters() {
 
 	r := gin.Default()
-	r.GET("/hello", helloHandler)
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/hello", helloHandler)
+		v1.POST("/employee", addEmployee)
+	}
+
 	r.Run()
 
 }
@@ -18,5 +28,20 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hello, Valhalla",
 	})
+}
 
+func addEmployee(c *gin.Context) {
+	db, err := dao.InitDb()
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	emp := model.Employee{}
+	reqBody, _ := ioutil.ReadAll(c.Request.Body)
+	json.Unmarshal(reqBody, &emp)
+
+	dao.AddEmployee(emp, db)
+
+	db.Close()
 }
